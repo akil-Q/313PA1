f87faa58034782e29b62f4b67a9ef9b2b097e8e2
Akil Daresalamwala <akildaresalamwala@Akils-MacBook-Pro-4.local>
2024-09-28 09:55:37 -0500

    Task 1

---
+++ client.cpp
@@ -0,0 +1,99 @@
+/*
+	Author of the starter code
+    Yifan Ren
+    Department of Computer Science & Engineering
+    Texas A&M University
+    Date: 9/15/2024
+	
+	Please include your Name, UIN, and the date below
+	Name: Akil Daresalamwala
+	UIN: 632006030
+	Date: 9/25/2024
+*/
+#include "common.h"
+#include "FIFORequestChannel.h"
+
+using namespace std;
+
+
+int main (int argc, char *argv[]) {
+	int opt;
+	int p = 1;
+	double t = 0.0;
+	int e = 1;
+	string filename = "";
+	int  m = MAX_MESSAGE;
+	//Add other arguments here
+	while ((opt = getopt(argc, argv, "p:t:e:f:")) != -1) {
+		switch (opt) {
+			case 'p':
+				p = atoi (optarg);
+				break;
+			case 't':
+				t = atof (optarg);
+				break;
+			case 'e':
+				e = atoi (optarg);
+				break;
+			case 'f':
+				filename = optarg;
+				break;
+			case 'm':
+			m = atoi(optarg);
+			break;	
+		}
+	}
+	
+	//Task 1:
+	//Run the server process as a child of the client process
+	// give arguments for the server, server needs './server', '-m'm '<val for -m arg>', 'NULL'
+	// fork
+	// In the childm run execvp using the server arguments.
+
+    FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
+	pid_t server = fork();
+
+	if (server == 0 ){
+		string m_s = to_string(m);
+		char* server_arg[] = {(char*)"./server", (char*)"-m",(char*)m_s.c_str() , NULL};
+		execvp(server_arg[0], server_arg);
+
+	} else {
+		cerr << "failed to fork" << endl;
+	}
+	//Task 4:
+	//Request a new channel
+	
+	//Task 2:
+	//Request data points
+    char buf[MAX_MESSAGE];
+    datamsg x(p, t, e);
+	
+	memcpy(buf, &x, sizeof(datamsg));
+	chan.cwrite(buf, sizeof(datamsg));
+	double reply;
+	chan.cread(&reply, sizeof(double));
+	cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
+	
+
+	//Task 3:
+	//Request files
+	filemsg fm(0, 0);
+	string fname = "1.csv";
+	
+	int len = sizeof(filemsg) + (fname.size() + 1);
+	char* buf2 = new char[len];
+	memcpy(buf2, &fm, sizeof(filemsg));
+	strcpy(buf2 + sizeof(filemsg), fname.c_str());
+	chan.cwrite(buf2, len);
+
+	delete[] buf2;
+	__int64_t file_length;
+	chan.cread(&file_length, sizeof(__int64_t));
+	cout << "The length of " << fname << " is " << file_length << endl;
+	
+	//Task 5:
+	// Closing all the channels
+    MESSAGE_TYPE m = QUIT_MSG;
+    chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+}
